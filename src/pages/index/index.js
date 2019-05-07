5292a85f74853095f44ca759bca37d6fd6d8c5d1
hellojer <hellojer@163.com>
2019-05-07 16:23:18 +0800

    init

---
+++ src/pages/index/index.js
@@ -0,0 +1,76 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
+import { AtButton, AtList, AtListItem } from 'taro-ui'
+
+import './index.scss'
+
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
+class Index extends Component {
+
+    config = {
+    navigationBarTitleText: '首页'
+  }
+
+  state = {
+    products: []
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  async componentWillMount() {
+    const response = await Taro.request({
+      url: `${ API_WS }/products`
+    })
+
+    this.setState({
+      products: response.data
+    })
+  }
+
+  render () {
+    const { products } = this.state
+
+    return (
+      <View className='index'>
+        <AtList>
+          {
+            products.map(product =>
+              <AtListItem
+                key={ product.id }
+                arrow='right'
+                title={ product.name }
+                note={ '￥' + product.price } />
+            )
+          }
+        </AtList>
+      </View>
+    )
+  }
+}
+
+export default Index
